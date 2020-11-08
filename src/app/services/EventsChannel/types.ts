9bda4f12a0f780a6ddb371553ad195072ff22ea8
Rickard Andersson <gonz@severnatazvezda.com>
2020-11-08 13:04:15 +0200

    rename `InPing` to `Ping`
    Will probably remove it eventually as it's not exactly
    a thing that matters manually anyway...

--- src/app/services/EventsChannel/types.ts
+++ src/app/services/EventsChannel/types.ts
@@ -1,13 +1,13 @@
 import * as t from "io-ts";
 
-export const InPing = t.type({type: t.literal("InPing")}, "InPing");
+export const Ping = t.type({type: t.literal("Ping")}, "Ping");
 
-export type InPing = t.TypeOf<typeof InPing>;
+export type Ping = t.TypeOf<typeof Ping>;
 
 export const Connected = t.type({type: t.literal("Connected")}, "Connected");
 
 export type Connected = t.TypeOf<typeof Connected>;
 
-export const IncomingEventsChannelEvent = t.union([InPing, Connected]);
+export const IncomingEventsChannelEvent = t.union([Ping, Connected]);
 
 export type IncomingEventsChannelEvent = t.TypeOf<typeof IncomingEventsChannelEvent>;
