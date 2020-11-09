63598ded53da8b143b134fb27e0f1028a1521ade
Rickard Andersson <gonz@severnatazvezda.com>
2020-11-10 00:51:22 +0200

    add the tracking types to package
    A `Stream` contains `Event`s.

--- src/app/services/EventsChannel/types.ts
+++ src/app/services/EventsChannel/types.ts
@@ -1,4 +1,5 @@
 import * as t from "io-ts";
+import {TrackingStream} from "../../../tracking/types";
 
 export const Ping = t.type({type: t.literal("Ping")}, "Ping");
 
@@ -8,6 +9,13 @@ export const Connected = t.type({type: t.literal("Connected")}, "Connected");
 
 export type Connected = t.TypeOf<typeof Connected>;
 
-export const IncomingEventsChannelEvent = t.union([Ping, Connected]);
+export const InitialStreams = t.type({
+  type: t.literal("InitialStreams"),
+  streams: t.array(TrackingStream),
+});
+
+export type InitialStreams = t.TypeOf<typeof InitialStreams>;
+
+export const IncomingEventsChannelEvent = t.union([Ping, Connected, InitialStreams]);
 
 export type IncomingEventsChannelEvent = t.TypeOf<typeof IncomingEventsChannelEvent>;
---
+++ src/tracking/types.ts
@@ -0,0 +1,37 @@
+import * as t from "io-ts";
+
+export const ExtraAnchorData = t.type({});
+export type ExtraAnchorData = t.TypeOf<typeof ExtraAnchorData>;
+
+export const ExtraHeadingData = t.type({});
+export type ExtraHeadingData = t.TypeOf<typeof ExtraHeadingData>;
+
+export const ExtraImageData = t.type({});
+export type ExtraImageData = t.TypeOf<typeof ExtraImageData>;
+
+export const ExtraData = t.union([ExtraAnchorData, ExtraHeadingData, ExtraImageData]);
+export type ExtraData = t.TypeOf<typeof ExtraData>;
+
+export const Parameters = t.type({url: t.string, elementSelector: t.string, extras: ExtraData});
+export type Parameters = t.TypeOf<typeof Parameters>;
+
+export const MouseOver = t.type({name: t.literal("mouseover"), parameters: Parameters});
+export type MouseOver = t.TypeOf<typeof MouseOver>;
+
+export const Load = t.type({name: t.literal("load"), parameters: Parameters});
+export type Load = t.TypeOf<typeof Load>;
+
+export const Click = t.type({name: t.literal("click"), parameters: Parameters});
+export type Click = t.TypeOf<typeof Click>;
+
+export const TrackingEvent = t.union([MouseOver, Click, Load]);
+export type TrackingEvent = t.TypeOf<typeof TrackingEvent>;
+
+export const TrackingStream = t.type({
+  id: t.string,
+  ip: t.string,
+  events: t.array(TrackingEvent),
+  inserted_at: t.string,
+  updated_at: t.string,
+});
+export type TrackingStream = t.TypeOf<typeof TrackingStream>;
