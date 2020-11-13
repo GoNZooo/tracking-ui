0227c3c9b6754754065af5c8dd9d8c2c6854f674
Rickard Andersson <gonz@severnatazvezda.com>
2020-11-13 18:11:37 +0200

    add some ngrx packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,18 @@ import {AppComponent} from "./app.component";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {ConfigurationService} from "./services/Configuration/configuration.service";
 import {StreamDetailsComponent} from "./components/stream-details/stream-details.component";
+import {StoreModule} from "@ngrx/store";
+import {EffectsModule} from "@ngrx/effects";
 
 @NgModule({
   declarations: [AppComponent, StreamDetailsComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+  ],
   providers: [ConfigurationService],
   bootstrap: [AppComponent],
 })
